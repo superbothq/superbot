180e4e1834db3b724b070dc1bf66e6143ae3cde5
Matti Paksula <matti.paksula@iki.fi>
2018-06-02 14:24:03 +0300

    chore (spec) passing.

--- spec/superbot_spec.rb
+++ spec/superbot_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe Superbot do
   it "has a version number" do
-    expect(Superbot::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(Superbot::VERSION).to be_a String
   end
 end
