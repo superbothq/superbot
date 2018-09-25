5c5de9cac1bf45ca5e4c4a59360bf512cd4d7928
DeeMak13 <dima.m@active-bridge.com>
2018-09-25 13:23:03 +0300

    Rework runner spec

--- spec/superbot/capybara/runner_spec.rb
+++ spec/superbot/capybara/runner_spec.rb
@@ -5,29 +5,27 @@ RSpec.describe Superbot::Capybara::Runner do
     let(:script) do
       [
         "visit 'http://www.example.com'",
-        "click_on 'Link'",
-        "page.execute_script('window.scrollBy(0,' + (page.execute_script('return document.body.scrollHeight') * 10 / 100).to_s + ')')",
-        "page.driver.browser.manage.window.resize_to(1920,1080)"
+        "page.driver.browser.close"
       ].join('; ')
     end
 
     context "when sb-capybara installed" do
-      let(:kommando) { instance_double(Kommando) }
+      context "when valid capybara script" do
+        it "succesfully runs script with sb-capybara" do
+          expect { described_class.run(script) }.to output("Test succeed\n").to_stdout
+        end
+      end
 
-      it "runs script with sb-capybara" do
-        expect_any_instance_of(Object).to receive(:gem).with('superbot-capybara').and_return(true)
-        expect(Kommando).to receive(:new).with('sb-capybara').and_return(kommando)
-        expect(kommando).to receive_message_chain(:in, :writeln).with({eval: script}.to_json)
-        expect(kommando).to receive_message_chain(:out, :once).and_yield
-        expect(kommando).to receive(:run_async).and_return(Thread.new { true })
-        expect(kommando).to receive(:kill).and_return(true)
-
-        expect { described_class.run(script) }.not_to output("superbot-capybara not installed\n").to_stdout
+      context "when invalid script" do
+        it "fails to execute script" do
+          expect { described_class.run('invalid') }.to output(/Test failed.*/).to_stdout
+        end
       end
     end
 
     context "when superbot-capybara not installed" do
       it 'returns notification' do
+        expect_any_instance_of(Object).to receive(:gem).with('superbot-capybara').and_raise(Gem::LoadError)
         expect { described_class.run(script) }.to output("superbot-capybara not installed\n").to_stdout
       end
     end
